3dd5cd710b9dda1f035d722262b7ac7c143f805a
Tabish Gillani <tabish@turnlunchon.com>
2018-04-04 17:22:55 +0500

    making something

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ItemsList from './components/ItemsList'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="panel-body" style={{width: '300px',marginLeft: '500px',marginTop: '100px'}}>
+            <div className="form-group">
+                <h4>Filter Items:</h4>
+                <input type="text" className="form-control" placeholder="search..." />
+            </div>
+            <div className="form-group">
+                <button className="btn btn-primary" style={{marginLeft: '110px'}}>Search</button>
+            </div>
+        </div>
+
     );
   }
 }
---
+++ src/components/ItemsList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class ItemsList extends Component
+{
+    render(){
+        return(
+        <div className="panel panel-primary">
+            <div className="panel-body">
+                
+            </div>
+        </div>
+
+        );
+    }
+}
+export default ItemsList;
